ab83ae9522259c577b95d2c2b45aec51ba5227b4
Kevin Ruff <34070651+ruffk@users.noreply.github.com>
2021-07-30 09:17:50 -0400

    Upgrade to SharpPcap 6.0.0 (#23)
    * Testing
    
    * Add solution
    
    * Add solution
    
    * Upgrade to SharpPcap 6.0.0
    
    * Upgrade to SharpPcap 6.0.0
    
    * Change TargetFramework to net5.0
    
    * Minor changes for repo pull request.

--- demo/Program.cs
+++ demo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using SharpPcap;
+using SharpPcap.LibPcap;
 using ZwiftPacketMonitor;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -12,9 +13,9 @@ namespace ZwiftPacketMonitorDemo
         {
             var serviceCollection = new ServiceCollection();
 
-            // added to allow logging level configuration
+            // added to allow logging level configuration, change MinLevel to LogLevel.Debug to see internal traces.
             serviceCollection.AddLogging(configure => configure.AddConsole())
-                .Configure<LoggerFilterOptions>(configure => configure.MinLevel = LogLevel.Debug);
+                .Configure<LoggerFilterOptions>(configure => configure.MinLevel = LogLevel.Information);
 
             serviceCollection.AddZwiftPacketMonitoring();
 
@@ -23,26 +24,69 @@ namespace ZwiftPacketMonitorDemo
             var logger = serviceProvider.GetService<ILogger<Program>>();
             var monitor = serviceProvider.GetService<Monitor>();
 
-            monitor.IncomingPlayerEvent += (s, e) => {
+            monitor.IncomingPlayerEvent += (s, e) =>
+            {
                 logger.LogInformation($"INCOMING: {e.PlayerState}");
             };
-            monitor.OutgoingPlayerEvent += (s, e) => {
+            monitor.OutgoingPlayerEvent += (s, e) =>
+            {
                 logger.LogInformation($"OUTGOING: {e.PlayerState}");
             };
-            monitor.IncomingChatMessageEvent += (s, e) => {
+            monitor.IncomingChatMessageEvent += (s, e) =>
+            {
                 logger.LogInformation($"CHAT: {e.Message}");
             };
-            monitor.IncomingPlayerEnteredWorldEvent += (s, e) => {
+            monitor.IncomingPlayerEnteredWorldEvent += (s, e) =>
+            {
                 logger.LogInformation($"WORLD: {e.PlayerUpdate}");
             };
-            monitor.IncomingRideOnGivenEvent += (s, e) => {
+            monitor.IncomingRideOnGivenEvent += (s, e) =>
+            {
                 logger.LogInformation($"RIDEON: {e.RideOn}");
             };
 
+            // Print SharpPcap version
+            var ver = Pcap.SharpPcapVersion;
+            Console.WriteLine("SharpPcap {0}, ZwiftPacketMonitor.Demo", ver);
+
+            // Retrieve the device list
+            var devices = LibPcapLiveDeviceList.Instance;
+
+            // If no devices were found print an error
+            if (devices.Count < 1)
+            {
+                Console.WriteLine("No devices were found on this machine");
+                return;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("The following devices are available on this machine:");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine();
+
+            int i = 0;
+
+            // Print out the devices
+            foreach (var dev in devices)
+            {
+                /* Description */
+                Console.WriteLine("{0}) {1} {2} [{3}]", i, dev.Name, dev.Description, dev.Interface.FriendlyName);
+                i++;
+            }
+
+            Console.WriteLine();
+            Console.Write("-- Please choose a device to capture: ");
+            i = int.Parse(Console.ReadLine());
+
             // network interface name or IP address (windows only)
-            monitor.StartCaptureAsync("en0").Wait();
+            _ = monitor.StartCaptureAsync(devices[i].Interface.Name); // not waiting to complete as it doesn't until StopCaptureAsync is called, instead wait for a keypress
+
+            // waiting for a keypress to shutdown clean
+            Console.WriteLine();
+            Console.WriteLine("Press ctrl-c to quit.");
+            Console.ReadLine();
           
-            // This won't get called until the above Wait finishes
+            // Stop monitoring
             monitor.StopCaptureAsync().Wait();
         }
     }
