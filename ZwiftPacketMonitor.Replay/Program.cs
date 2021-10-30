da7941fdb729a535d754440a7cf246db74966147
Sander van Vliet <sander@codenizer.nl>
2021-10-30 14:14:27 +0200

    Use input file name to determine packet output directory

--- ZwiftPacketMonitor.Replay/Program.cs
+++ ZwiftPacketMonitor.Replay/Program.cs
@@ -29,7 +29,7 @@ namespace ZwiftPacketMonitor.Replay
 
             var logger = serviceProvider.GetService<ILogger<Program>>();
             var replay = serviceProvider.GetService<Replayer>();
-            serviceProvider.GetRequiredService<MessageDiagnostics>().OutputTo($"c:\\git\\temp\\zwift\\companion-04-tcp");
+            serviceProvider.GetRequiredService<MessageDiagnostics>().OutputTo(Path.Combine(Path.GetDirectoryName(path), Path.GetFileNameWithoutExtension(path)));
 
             replay.IncomingPlayerEvent += (s, e) =>
             {
